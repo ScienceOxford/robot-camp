23adf55a772585bdcd887cbf194cd12a47d899b6
Sarah Townson <sarah.townson@scienceoxford.com>
2018-08-08 14:17:29 +0100

    Update sensors_starter.py

--- day-4/sensors_starter.py
+++ day-4/sensors_starter.py
@@ -20,29 +20,28 @@ def stop():
     LB.write_analog(1023)
     RF.write_analog(1023)
     RB.write_analog(1023)
-    display.show(Image.ANGRY)
 
 
 # The L9110s motor driver uses an input of 0 (low) to turn the motor on at full speed, and an input of 1023 (high) to turn it off
 # The below function flips this so that when calling drive(), a high number runs the motors faster
-def drive(L, R):
+def drive(left_motor, right_motor):
     # Below controls the left wheel: forward, backward, stop at given speed
-    if L > 0 and L <= 1023:
-        LF.write_analog(abs(L-1023))  # go forwards at speed given
+    if left_motor > 0 and left_motor <= 1023:
+        LF.write_analog(abs(left_motor-1023))  # go forwards at speed given
         LB.write_analog(1023)         # don't go backwards
-    elif L < 0 and L >= -1023:
+    elif left_motor < 0 and left_motor >= -1023:
         LF.write_analog(1023)         # don't go forwards
-        LB.write_analog(abs(L+1023))  # go backwards at speed given
+        LB.write_analog(abs(left_motor+1023))  # go backwards at speed given
     else:
         LF.write_analog(1023)         # stop the left wheel
         LB.write_analog(1023)
     # Below controls the right wheel: forward, backward, stop at given speed
-    if R > 0 and R <= 1023:
-        RF.write_analog(abs(R-1023))  # go forwards at speed given
+    if right_motor > 0 and right_motor <= 1023:
+        RF.write_analog(abs(right_motor-1023))  # go forwards at speed given
         RB.write_analog(1023)         # don't go backwards
-    elif R < 0 and R >= -1023:
+    elif right_motor < 0 and right_motor >= -1023:
         RF.write_analog(1023)         # don't go forwards
-        RB.write_analog(abs(R+1023))  # go backwards at speed given
+        RB.write_analog(abs(right_motor+1023))  # go backwards at speed given
     else:
         RF.write_analog(1023)         # stop the right wheel
         RB.write_analog(1023)
